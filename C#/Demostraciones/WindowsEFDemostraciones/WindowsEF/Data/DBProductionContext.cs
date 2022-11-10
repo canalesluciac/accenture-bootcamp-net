12fd616affb5de501a84498d1a2050a15c609c81
canalesluciac <canalesluciac@gmail.com>
2022-11-10 19:57:59 -0300

    add Práctica Crear ORM desde la base de datos Pubs

--- C#/Demostraciones/WindowsEFDemostraciones/WindowsEF/Data/DBProductionContext.cs
+++ C#/Demostraciones/WindowsEFDemostraciones/WindowsEF/Data/DBProductionContext.cs
@@ -17,7 +17,33 @@ namespace WindowsEF.Data
        // por cada modelo una propiedad DBSET
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Producto> Productos { get; set; }
+        public DbSet<Empleado> Empleados { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeDetail> EmployeeDetails { get; set; }
 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
 
+            //Configuración mapeo con FLUENT API
+
+            //PK
+            modelBuilder.Entity<Empleado>().HasKey(e => e.SID);
+
+            //Campo obligatorio
+            modelBuilder.Entity<Empleado>().Property(e => e.Nombre).IsRequired();
+
+            //Tipo de dato
+            modelBuilder.Entity<Empleado>().Property(e => e.Nombre).HasColumnType("varchar");
+
+            //Longitud de la cadena
+            modelBuilder.Entity<Empleado>().Property(e => e.Nombre).HasMaxLength(50);
+
+            //Nombre tabla
+            modelBuilder.Entity<Empleado>().ToTable("Empleado");
+
+            //Relacion de 1 a 1
+            modelBuilder.Entity<Employee>().HasOptional(e => e.EmployeeDetail).WithRequired(e => e.Employee);
+        }
     }
 }
